295ee4786adafda8a1c68460c82430edcde4da66
PTARSONEVES <ptarsoneves@gmail.com>
2025-01-16 12:54:03 -0300

    Limpeza e styled.

---
+++ src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Title } from "./styled";
+
+export default function Login() {
+  return (
+    <>
+     <Title>
+      Login
+      <small>Testando</small>
+     </Title>
+     <p>Loren lnerjhgmsççgjj</p>
+    </>
+  );
+}
